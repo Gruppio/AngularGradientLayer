67948410c481f8a2a8f5fc0a60ad5bf3d719e71f
Michele Gruppioni <gruppiofigo@hotmail.it>
2015-11-14 00:24:11 +0100

    Updated Pod in the Example

--- AngularGradientLayerExample/AngularGradientLayerExample/ViewController.swift
+++ AngularGradientLayerExample/AngularGradientLayerExample/ViewController.swift
@@ -13,8 +13,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        let angularGradientLayer = AngularGradientLayer()
-        // Do any additional setup after loading the view, typically from a nib.
+        let angularGradientLayer = AngularGradientLayer()        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
